a04938c55b7f6f06f91bcf8598bb4f64b3e0c5da
Haifeng <157823459@qq.com>
2019-05-10 21:10:03 +0800

    Initial commit

---
+++ lib/MoviesPage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MoviesPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+
+}
+
+
+
+class _MoviesPage extends State<MoviesPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+
+      body: ,
+
+    );
+  }
+
+}
